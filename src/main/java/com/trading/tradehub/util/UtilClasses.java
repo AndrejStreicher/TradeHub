4bc920439c8644817f1123003e0e75ece1d04597
Andrej Streicher <andrejstreicher1@gmail.com>
2023-07-30 23:43:16 +0200

    Name change

--- src/main/java/com/trading/tradehub/util/UtilClasses.java
+++ src/main/java/com/trading/tradehub/util/UtilClasses.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class utilClasses
+public class UtilClasses
 {
     // Converts an HTML file into a string
     public static String HTMLFileToString(String filePath)
