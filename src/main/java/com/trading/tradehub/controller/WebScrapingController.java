30308183c27eca161b75a3a2c5caa76ced700288
Andrej Streicher <andrejstreicher1@gmail.com>
2023-07-30 18:58:02 +0200

    Initial commit

---
+++ src/main/java/com/trading/tradehub/controller/WebScrapingController.java
@@ -0,0 +1,66 @@
+package com.trading.tradehub.controller;
+
+import com.trading.tradehub.model.ClusterInsiderBuysModel;
+import com.trading.tradehub.model.TickerInsiderTradeModel;
+import com.trading.tradehub.service.OpenInsiderWebScraperService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/webscrape")
+public class WebScrapingController
+{
+
+    private final OpenInsiderWebScraperService openInsiderWebScraperService;
+
+    @Autowired
+    public WebScrapingController(OpenInsiderWebScraperService openInsiderWebScraperService)
+    {
+        this.openInsiderWebScraperService = openInsiderWebScraperService;
+    }
+
+    /**
+     * Endpoint to retrieve the latest cluster insider buys.
+     *
+     * @return ResponseEntity containing a list of ClusterInsiderBuysModel if available, else a 404 Not Found response.
+     */
+    @GetMapping("openinsider/clusterbuys")
+    public ResponseEntity<List<ClusterInsiderBuysModel>> getClusterBuys()
+    {
+        List<ClusterInsiderBuysModel> clusterBuys = openInsiderWebScraperService.scrapeLatestClusterBuys();
+        if (clusterBuys.isEmpty())
+        {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(clusterBuys);
+    }
+
+    /**
+     * Endpoint to retrieve the latest insider buys for a specific ticker.
+     *
+     * @param ticker The ticker symbol for the stock.
+     * @return ResponseEntity containing a list of TickerInsiderBuysModel if available, else a 404 Not Found response.
+     * If the ticker is empty or null, a 400 Bad Request response is returned.
+     */
+    @GetMapping("openinsider/{ticker}")
+    public ResponseEntity<List<TickerInsiderTradeModel>> getTickerInsiderTrades(@PathVariable String ticker)
+    {
+        // Input validation: Check if the ticker is not empty or null
+        if (ticker == null || ticker.trim().isEmpty())
+        {
+            // If the ticker is invalid, return a 400 Bad Request response to the client
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<TickerInsiderTradeModel> insiderTrades = openInsiderWebScraperService.scrapeLatestTickerClusterBuys(ticker);
+        // Return the response with the list of TickerInsiderBuysModel
+        return ResponseEntity.ok(insiderTrades);
+    }
+
+}
---
+++ src/test/java/com/trading/tradehub/TelegramMessageTests.java
@@ -0,0 +1,50 @@
+package com.trading.tradehub;
+
+import com.example.demo.model.ClusterInsiderBuysModel;
+import com.example.demo.service.TelegramMessageService;
+import com.trading.tradehub.model.ClusterInsiderBuysModel;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@SpringBootTest
+class TelegramMessageTests
+{
+    private final TelegramMessageService telegramMessageService;
+
+    @Autowired
+    public TelegramMessageTests(TelegramMessageService telegramMessageService)
+    {
+        this.telegramMessageService = telegramMessageService;
+    }
+
+    @Test
+    void groupMessageClusterBuyTest()
+    {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        ClusterInsiderBuysModel clusterInsiderBuysModelTest = new ClusterInsiderBuysModel(
+                "2M",
+                LocalDateTime.parse("2023-07-26 12:04:47", dateTimeFormatter),
+                LocalDate.parse("2023-07-25", dateFormatter),
+                "TEST",
+                "Test Company",
+                "Testing Industry",
+                4,
+                "P - Purchase",
+                4.50,
+                100,
+                150,
+                16,
+                4560320);
+        HttpResponse<String> response = telegramMessageService.sendMessage(TelegramMessageService.TargetChat.TEST, clusterInsiderBuysModelTest);
+        Assertions.assertNotNull(response);
+        Assertions.assertTrue(response.toString().contains("200"));
+    }
+}
