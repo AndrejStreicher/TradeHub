c129b615cab6dd62cb9741906e83eb4e809020bc
Andrej Streicher <andrejstreicher1@gmail.com>
2023-08-04 03:23:15 +0200

    Reworked tests using mocking.

--- src/test/java/com/trading/tradehub/TelegramBotServiceTests.java
+++ src/test/java/com/trading/tradehub/TelegramBotServiceTests.java
@@ -1,72 +1,8 @@
 package com.trading.tradehub;
 
-import com.trading.tradehub.model.ClusterInsiderBuysModel;
-import com.trading.tradehub.service.TelegramBotService;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.net.http.HttpResponse;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @SpringBootTest
 class TelegramBotServiceTests
 {
-    private final TelegramBotService telegramBotService;
-    private final ClusterInsiderBuysModel clusterInsiderBuysModelTest;
-
-    @Autowired
-    public TelegramBotServiceTests(TelegramBotService telegramBotService)
-    {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        clusterInsiderBuysModelTest = new ClusterInsiderBuysModel(
-                "2M",
-                LocalDateTime.parse("2023-07-26 12:04:47", dateTimeFormatter),
-                LocalDate.parse("2023-07-25", dateFormatter),
-                "TEST",
-                "Test Company",
-                "Testing Industry",
-                4,
-                "P - Purchase",
-                4.50,
-                100,
-                150,
-                16,
-                4560320);
-        this.telegramBotService = telegramBotService;
-    }
-
-    @Test
-    void groupMessageClusterBuyTest()
-    {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        ClusterInsiderBuysModel clusterInsiderBuysModelTest = new ClusterInsiderBuysModel(
-                "2M",
-                LocalDateTime.parse("2023-07-26 12:04:47", dateTimeFormatter),
-                LocalDate.parse("2023-07-25", dateFormatter),
-                "TEST",
-                "Test Company",
-                "Testing Industry",
-                4,
-                "P - Purchase",
-                4.50,
-                100,
-                150,
-                16,
-                4560320);
-        HttpResponse<String> response = telegramBotService.sendMessage(TelegramBotService.TargetChat.TEST, clusterInsiderBuysModelTest);
-        Assertions.assertNotNull(response);
-        Assertions.assertEquals(200, response.statusCode());
-    }
-
-    @Test
-    void getUpdatesTest()
-    {
-        System.out.println(telegramBotService.getBotUpdates() + "___________");
-    }
 }
--- src/test/java/com/trading/tradehub/WebScrapingControllerTests.java
+++ src/test/java/com/trading/tradehub/WebScrapingControllerTests.java
@@ -1,62 +1,162 @@
 package com.trading.tradehub;
 
+import com.trading.tradehub.controller.WebScrapingController;
+import com.trading.tradehub.model.ClusterInsiderBuysModel;
+import com.trading.tradehub.model.TickerInsiderTradeModel;
+import com.trading.tradehub.service.OpenInsiderWebScraperService;
+import com.trading.tradehub.service.YahooFinanceWebScraperService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.*;
 
-@SpringBootTest
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(WebScrapingController.class)
 class WebScrapingControllerTests
 {
-    private final HttpClient httpClient = HttpClient.newHttpClient();
-    @Value("${baseUrl}")
-    private String baseUrl;
+    @MockBean
+    private OpenInsiderWebScraperService openInsiderWebScraperService;
+    @MockBean
+    private YahooFinanceWebScraperService yahooFinanceWebScraperService;
+
+    @Autowired
+    private WebScrapingController webScrapingController;
 
     @Test
-    void latestClusterBuyWebScraperTest()
+    void getLatestClusterBuys_ReturnsNonEmptyList_StatusCode200()
     {
-        String URIString = baseUrl + "/webscrape/openinsider/clusterbuys";
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URIString))
-                .GET()
-                .build();
+        // Create mock data
+        List<ClusterInsiderBuysModel> clusterInsiderBuysModels = new ArrayList<>();
+        clusterInsiderBuysModels.add(new ClusterInsiderBuysModel(
+                "M",
+                LocalDateTime.now(),
+                LocalDate.of(2023, 8, 1),
+                "AAPL",
+                "Apple Inc.",
+                "Technology",
+                5,
+                "Buy",
+                145.67,
+                1000,
+                2000,
+                100,
+                145670
+        ));
 
-        try
-        {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            Assertions.assertNotNull(response);
-            Assertions.assertEquals(200, response.statusCode());
+        when(openInsiderWebScraperService.scrapeLatestClusterBuys()).thenReturn(clusterInsiderBuysModels);
 
-        } catch (IOException | InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+        ResponseEntity<List<ClusterInsiderBuysModel>> response = webScrapingController.getLatestClusterBuys();
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(clusterInsiderBuysModels, response.getBody());
     }
 
     @Test
-    void tickerLatestInsiderBuysTest()
+    void getLatestClusterBuys_ReturnsEmptyList_StatusCode400()
     {
-        String URIString = baseUrl + "/webscrape/openinsider/AAPL";
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(URIString))
-                .GET()
-                .build();
+        List<ClusterInsiderBuysModel> clusterInsiderBuysModels = Collections.emptyList();
+        when(openInsiderWebScraperService.scrapeLatestClusterBuys()).thenReturn(clusterInsiderBuysModels);
+        ResponseEntity<List<ClusterInsiderBuysModel>> response = webScrapingController.getLatestClusterBuys();
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertNull(response.getBody());
+    }
 
-        try
-        {
-            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-            Assertions.assertNotNull(response);
-            Assertions.assertEquals(200, response.statusCode());
+    @Test
+    void getTickerInsiderTrades_ReturnsNonEmptyList_StatusCode200()
+    {
+        List<TickerInsiderTradeModel> tickerInsiderTradeModels = new ArrayList<>();
+        tickerInsiderTradeModels.add(new TickerInsiderTradeModel(
+                "X",
+                LocalDateTime.now(),
+                LocalDate.of(2023, 8, 1),
+                "TEST",
+                "Chad Broski",
+                "CEO",
+                "P - Purchase",
+                6.52,
+                350,
+                23,
+                4,
+                23654));
+        when(openInsiderWebScraperService.scrapeLatestTickerClusterBuys("TEST")).thenReturn(tickerInsiderTradeModels);
 
-        } catch (IOException | InterruptedException e)
-        {
-            e.printStackTrace();
-        }
+        ResponseEntity<List<TickerInsiderTradeModel>> response = webScrapingController.getTickerInsiderTrades("TEST");
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(tickerInsiderTradeModels, response.getBody());
+    }
+
+    @Test
+    void getTickerInsiderTrades_ReturnsCollectionsDotEmptyList_StatusCode400()
+    {
+        List<TickerInsiderTradeModel> tickerInsiderTradeModels = Collections.emptyList();
+        when(openInsiderWebScraperService.scrapeLatestTickerClusterBuys("TEST")).thenReturn(tickerInsiderTradeModels);
+
+        ResponseEntity<List<TickerInsiderTradeModel>> response = webScrapingController.getTickerInsiderTrades("TEST");
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertNull(response.getBody());
+    }
+
+    @Test
+    void getTickerInsiderTrades_TickerIsNull_StatusCode400()
+    {
+        ResponseEntity<List<TickerInsiderTradeModel>> response = webScrapingController.getTickerInsiderTrades(null);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertNull(response.getBody());
+    }
+
+    @Test
+    void getTickerInsiderTrades_ReturnsEmptyArrayList_StatusIs204()
+    {
+        List<TickerInsiderTradeModel> tickerInsiderTradeModels = new ArrayList<>();
+        when(openInsiderWebScraperService.scrapeLatestTickerClusterBuys("TEST")).thenReturn(tickerInsiderTradeModels);
+
+        ResponseEntity<List<TickerInsiderTradeModel>> response = webScrapingController.getTickerInsiderTrades("TEST");
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+        Assertions.assertNull(response.getBody());
+    }
+
+    @Test
+    void getCurrentPrice_ReturnsNonNullOptionalDouble_StatusCode200()
+    {
+        Optional<Double> currentPrice = Optional.of(65.22);
+        when(yahooFinanceWebScraperService.getCurrentPrice("TEST")).thenReturn(currentPrice);
+
+        ResponseEntity<Optional<Double>> response = webScrapingController.getCurrentPrice("TEST");
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
+        Assertions.assertEquals(currentPrice, response.getBody());
+    }
+
+    @Test
+    void getCurrentPrice_ReturnsNullOptionalDouble_StatusCode400()
+    {
+        Optional<Double> currentPrice = Optional.empty();
+        when(yahooFinanceWebScraperService.getCurrentPrice("TEST")).thenReturn(currentPrice);
+        ResponseEntity<Optional<Double>> response = webScrapingController.getCurrentPrice("TEST");
+
+        Assertions.assertThrowsExactly(NoSuchElementException.class, currentPrice::get);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void getCurrentPrice_TickerIsNull_StatusCode400()
+    {
+        ResponseEntity<Optional<Double>> response = webScrapingController.getCurrentPrice(null);
+        Assertions.assertNotNull(response);
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        Assertions.assertNull(response.getBody());
     }
 }
