679a6ce6120f58ef0c3104e8c34370023e6776c1
Andrej Streicher <andrejstreicher1@gmail.com>
2023-08-16 02:55:50 +0200

    Open insider alert service tests

---
+++ src/test/java/com/trading/tradehub/OpenInsiderAlertServiceTests.java
@@ -0,0 +1,58 @@
+package com.trading.tradehub;
+
+import com.trading.tradehub.model.ClusterInsiderBuyModel;
+import com.trading.tradehub.service.OpenInsiderAlertService;
+import com.trading.tradehub.service.OpenInsiderWebScraperService;
+import com.trading.tradehub.service.TelegramBotService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+class OpenInsiderAlertServiceTests
+{
+    private OpenInsiderAlertService openInsiderAlertService;
+    @MockBean
+    private TelegramBotService telegramBotService;
+    @MockBean
+    private OpenInsiderWebScraperService openInsiderWebScraperService;
+
+    @BeforeEach
+    void setUp()
+    {
+        openInsiderAlertService = new OpenInsiderAlertService(telegramBotService, openInsiderWebScraperService);
+    }
+
+    @Test
+    void newClusterBuyAlert_NewClusterBuyAlert_TelegramMessageSent()
+    {
+        ClusterInsiderBuyModel mockedClusterModelInitial = Mockito.mock(ClusterInsiderBuyModel.class);
+        ClusterInsiderBuyModel mockedClusterModelLatest = Mockito.mock(ClusterInsiderBuyModel.class);
+        List<ClusterInsiderBuyModel> mockedClusterModelListInitial = new ArrayList<>();
+        List<ClusterInsiderBuyModel> mockedClusterModelListLatest = new ArrayList<>();
+        mockedClusterModelListInitial.add(mockedClusterModelInitial);
+        mockedClusterModelListLatest.add(mockedClusterModelLatest);
+
+        when(openInsiderWebScraperService.scrapeLatestClusterBuys()).thenReturn(mockedClusterModelListInitial);
+        openInsiderAlertService.setClusterBuyAlertEnabled(true);
+        openInsiderAlertService.newClusterBuyAlert();
+
+        // Assertions after the first call
+        verify(openInsiderWebScraperService, times(1)).scrapeLatestClusterBuys();
+
+        // Second call to newClusterBuyAlert
+        when(openInsiderWebScraperService.scrapeLatestClusterBuys()).thenReturn(mockedClusterModelListLatest);
+        openInsiderAlertService.newClusterBuyAlert();
+
+        // Assertions after the second call
+        verify(openInsiderWebScraperService, times(2)).scrapeLatestClusterBuys();
+        verify(telegramBotService, times(1)).sendMessage(TelegramBotService.TargetChat.GROUP, mockedClusterModelLatest);
+    }
+}
